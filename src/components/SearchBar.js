00b6c78b47e679ab9b176eb3269fcc5bf1f76054
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-23 21:04:41 +0100

    added Search bar complete

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,41 @@
 import React from "react";
 import { TextInput, View, Text, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
 
-const SearchBar = () => {
-    return <View style={styles.background}>
-        <Text>  Search Bar </Text>
+    return <View style={styles.backgroundStyle}>
+        <Feather name='search' style={styles.iconStyle} />
+        <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            value={term}
+            onChangeText={ onTermChange }
+            onEndEditing={() =>    onTermSubmit()  }
+            onSubmitEditing={onTermSubmit  }
+            placeholder="Search"
+            style={styles.inputStyle} />
     </View>
 }
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
         backgroundColor: '#F0EEEE'
         , height: 50,
         borderRadius: 5,
+        marginHorizontal: 15,
+        marginTop: 10,
+        flexDirection: 'row'
+
+    }
+    , inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        alignSelf: "center",
+        fontSize: 35
     }
 })
 export default SearchBar
-
-
-
 
 
 
--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
+
 const SearchScreen = () => {
+    const [term, setTerm] = useState('')
+    const [result, setResult] = useState('')
     return <View style={styles.backgroud}>
-        <SearchBar />
-        <Text> Search Scren</Text>
+        <SearchBar term={term}
+            onTermSubmit={() => console.log("Submit")}
+            onTermChange={(newTerm) => setTerm(newTerm)} />
+
+        <Text> Search Screen   </Text>
+        <Text>  {term}</Text>
     </View>
 }
 const styles = StyleSheet.create({
